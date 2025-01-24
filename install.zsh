1490995171f77b30520b86238707c09c409a33af
Park JongBeum <dev@parkjb.com>
2025-01-24 16:53:58 +0900

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -32,7 +32,7 @@ asdf install nodejs 22.13.0 && \
 asdf global nodejs 22.13.0'
 
 # asdf install python
-sudo apt-get update && apt-get install -y make build-essential libssl-dev zlib1g-dev \
+sudo apt-get update && sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
